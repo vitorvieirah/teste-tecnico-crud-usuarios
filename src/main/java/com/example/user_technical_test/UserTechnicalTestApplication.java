f85477c19d4a90e57d2d1869b62689f52b08cbda
VitorVieira <vitorhvvieira@gmail.com>
2024-10-04 14:43:52 -0300

    Subindo projeto, estrutura de pastas

---
+++ src/main/java/com/example/user_technical_test/UserTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_technical_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/user_technical_test/UserTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_technical_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
